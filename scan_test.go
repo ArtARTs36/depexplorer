dadf7b5cf2cda4b5781044b67e1868b51aea67c3
artarts36 <temicska99@mail.ru>
2024-12-07 03:20:10 +0300

    chore: test for scan projects

---
+++ scan_test.go
@@ -0,0 +1,15 @@
+package depexplorer_test
+
+import (
+	"github.com/artarts36/depexplorer"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestScanProjectThisRepository(t *testing.T) {
+	file, err := depexplorer.ScanProject("./")
+	require.NoError(t, err)
+	assert.Equal(t, "go.mod", file.Name)
+	assert.Equal(t, depexplorer.DependencyManagerGoMod, file.DependencyManager)
+}
