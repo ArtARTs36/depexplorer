6004bc333171b39456cff396993aaaba09dc8546
artarts36 <temicska99@mail.ru>
2024-12-15 04:21:58 +0300

    feat: add gitlab

---
+++ pkg/gitlab/client.go
@@ -0,0 +1,86 @@
+package gitlab
+
+import (
+	"context"
+	"fmt"
+	"github.com/artarts36/depexplorer"
+	"github.com/artarts36/depexplorer/pkg/repository"
+	"gitlab.com/gitlab-org/api/client-go"
+)
+
+type Client struct {
+	client *gitlab.Client
+}
+
+func NewClient(client *gitlab.Client) *Client {
+	return &Client{
+		client: client,
+	}
+}
+
+func NewClientWithToken(token string) (*Client, error) {
+	client, err := gitlab.NewClient(token)
+	if err != nil {
+		return nil, err
+	}
+	return NewClient(client), nil
+}
+
+func (c *Client) ListFiles(
+	ctx context.Context,
+	repo repository.Repo,
+	dir string,
+) (depexplorer.DirectoryFileIterator, error) {
+	projectID := c.createProjectID(repo)
+
+	nodes, _, err := c.client.Repositories.ListTree(projectID, &gitlab.ListTreeOptions{
+		Path: &dir,
+		ListOptions: gitlab.ListOptions{
+			PerPage: 100,
+		},
+	}, gitlab.WithContext(ctx))
+	if err != nil {
+		return nil, fmt.Errorf("list project tree: %w", err)
+	}
+
+	ref, err := c.getDefaultBranch(ctx, projectID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to default branch: %w", err)
+	}
+
+	return newFileIterator(nodes, func(path string) (*gitlab.File, error) {
+		node, _, fErr := c.client.RepositoryFiles.GetFile(projectID, "go.mod", &gitlab.GetFileOptions{
+			Ref: &ref,
+		}, gitlab.WithContext(ctx))
+
+		return node, fErr
+	}), nil
+}
+
+func (c *Client) getDefaultBranch(ctx context.Context, projectID string) (string, error) {
+	branches, _, err := c.client.Branches.ListBranches(projectID, &gitlab.ListBranchesOptions{
+		ListOptions: gitlab.ListOptions{
+			PerPage: 100,
+		},
+	}, gitlab.WithContext(ctx))
+	if err != nil {
+		return "", fmt.Errorf("failed to list repository branches: %w", err)
+	}
+
+	defaultBranch := ""
+	for _, branch := range branches {
+		if branch.Default {
+			defaultBranch = branch.Name
+		}
+	}
+
+	if defaultBranch == "" {
+		return "", fmt.Errorf("default branch not found")
+	}
+
+	return defaultBranch, nil
+}
+
+func (c *Client) createProjectID(repo repository.Repo) string {
+	return fmt.Sprintf("%s/%s", repo.Owner, repo.Name)
+}
---
+++ pkg/gitlab/iterator.go
@@ -0,0 +1,67 @@
+package gitlab
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"io"
+
+	gitlab "gitlab.com/gitlab-org/api/client-go"
+)
+
+type fileLoader func(path string) (*gitlab.File, error)
+
+type fileIterator struct {
+	files      []*gitlab.TreeNode
+	fileLoader fileLoader
+
+	index int
+}
+
+func newFileIterator(
+	files []*gitlab.TreeNode,
+	fileLoader fileLoader,
+) *fileIterator {
+	return &fileIterator{
+		files:      files,
+		index:      -1,
+		fileLoader: fileLoader,
+	}
+}
+
+func (i *fileIterator) Next() (string, error) {
+	i.index++
+	if i.index >= len(i.files)-1 {
+		return "", io.EOF
+	}
+
+	return i.files[i.index].Path, nil
+}
+
+func (i *fileIterator) Read(filepath string) ([]byte, error) {
+	file, err := i.fileLoader(filepath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to load file: %w", err)
+	}
+
+	fileContent, err := getFileContent(file)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode file %q: %v", filepath, err)
+	}
+
+	return fileContent, nil
+}
+
+func getFileContent(file *gitlab.File) ([]byte, error) {
+	switch file.Encoding {
+	case "base64":
+		c, err := base64.StdEncoding.DecodeString(file.Content)
+		return c, err
+	case "":
+		return []byte(file.Content), nil
+	case "none":
+		return nil, errors.New("unsupported content encoding: none, this may occur when file size > 1 MB, if that is the case consider using DownloadContents")
+	default:
+		return nil, fmt.Errorf("unsupported content encoding: %v", file.Encoding)
+	}
+}
